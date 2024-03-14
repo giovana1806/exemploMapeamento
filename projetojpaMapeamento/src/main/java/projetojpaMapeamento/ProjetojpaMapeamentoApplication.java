c5846bacaf65538fc39f483d4a9ab66ad4ffbde2
giovana1806 <132360035+giovana1806@users.noreply.github.com>
2024-03-14 12:34:55 -0300

    Add files via upload

---
+++ projetojpaMapeamento/src/main/java/projetojpaMapeamento/ProjetojpaMapeamentoApplication.java
@@ -0,0 +1,13 @@
+package projetojpaMapeamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaMapeamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaMapeamentoApplication.class, args);
+	}
+
+}
